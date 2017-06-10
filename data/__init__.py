df56c0cb2c391f32c760fcac39fc320452526670
itshakh <zahi.hershko@gmail.com>
2017-06-10 17:04:46 +0300

    Adjust data structure to our conventions including saving images in seperate folders along with the pickled data.

--- data/__init__.py
+++ data/__init__.py
@@ -18,6 +18,9 @@ class Data:
         self.val_x = None
         self.val_y = None
 
+        self.test_x = None
+        self.test_y = None
+
         self.train_x = None
         self.train_y = None
 
@@ -46,91 +49,46 @@ class Data:
         :param set_range:
         :return:
         """
-        return (self.x[set_range], self.y[set_range])
+
+        return self.x[set_range], self.y[set_range]
 
     def get_train_batch(self, set_range):
         """
-        returns a mini batch of data
-        :param kind: 'train', 'test' or 'validation'
+        Return a batch for training
         :param set_range:
         :return:
         """
-        return (self.train_x[set_range], self.train_y[set_range])
+        return self.train_x[set_range], self.train_y[set_range]
 
-    def get_val_batch(self, set_range):
+    def set_train_val_batch(self, val_amount, test_amount):
         """
-        returns a mini batch of data
-        :param kind: 'train', 'test' or 'validation'
-        :param set_range:
+
+        :param val_amount: number of validation images
+        :param test_amount: -"- test -"-
         :return:
         """
 
-        self.train_x = numpy.ndarray(shape=(self.x.shape[0] - len(list(set_range)), self.x.shape[1], self.x.shape[2], 3),dtype=numpy.float32)
-        self.train_y = numpy.ndarray(shape=(self.x.shape[0] - len(list(set_range)), 1), dtype=numpy.float32)
-        self.train_x[:set_range[0]] = self.x[:set_range[0]]
-        self.train_x[set_range[0]:] = self.x[set_range[-1]:]
+        self.train_x = self.x[:self.x.shape[0] - val_amount - test_amount]
+        self.train_y = self.y[:self.x.shape[0] - val_amount - test_amount]
 
-        self.train_y[:set_range[0]] = self.y[:set_range[0]]
-        self.train_y[set_range[0]:] = self.y[set_range[-1]:]
+        self.val_x = self.x[self.train_x.shape[0]: self.train_x.shape[0] + val_amount]
+        self.val_y = self.y[self.train_x.shape[0]: self.train_x.shape[0] + val_amount]
 
-        self.val_x = self.x[set_range]
-        self.val_y = self.y[set_range]
+        self.test_x = self.x[self.x.shape[0] - test_amount:self.x.shape[0]]
+        self.test_y = self.y[self.x.shape[0] - test_amount:self.x.shape[0]]
 
-        return self.val_x, self.val_y
-
-    def export_data(self, path):
+    def export_data(self, train_path, val_path, test_path):
         """
             save data as pickle
         """
-        pickle.dump((self.x, self.y), open( path, "wb" ) )
+        pickle.dump((self.train_x, self.train_y), open(train_path, "wb"))
+        pickle.dump((self.val_x, self.val_y), open(val_path, "wb"))
+        pickle.dump((self.train_x, self.train_y), open(test_path, "wb"))
 
-    def import_data(self, path):
+    def import_data(self, train_path, val_path, test_path):
         """
-            save data as pickle
+            load pickles
         """
-        (self.x, self.y) = pickle.load(open( path, "rb" ) )
-
-
-    def load_data(self, path_to_images, names_file_path, scores_file_path, patch_size=(32, 32)):
-        f1 = open(names_file_path)
-        names = f1.readlines()
-        f2 = open(scores_file_path)
-        scores = f2.readlines()
-        x = numpy.ndarray(shape=(len(names), patch_size[0], patch_size[1], 3),dtype=numpy.float32)
-        y = numpy.ndarray(shape=(len(names), 1), dtype=numpy.float32)
-
-        full_data = (x, y)
-        current_example = 0
-        for name, score, k in zip(names, scores, range(len(names))):
-            print("Load image number {:d} / {:d}".format(k, len(names)))
-            type = name.split('.')[-1].strip('\n')
-            # temp_image = functions.contrast_normalize(im.imread(os.path.join(path_to_images, name.strip('\n')), type))
-            temp_image = numpy.float32(im.imread(os.path.join(path_to_images, name.strip('\n')), type))
-            temp_score = numpy.float32(score)
-            slices = (int(temp_image.shape[0] / patch_size[0]), int(temp_image.shape[1] / patch_size[1]))
-
-            if x.shape[0] - current_example <= slices[0] * slices[1]:
-                temp = numpy.ndarray(shape=(x.shape[0] + (slices[0] * slices[1] - x.shape[0] + current_example),
-                                            patch_size[0], patch_size[1], 3), dtype=numpy.float32)
-                temp[:x.shape[0]] = x
-
-                x = temp
-
-                temp = numpy.ndarray(shape=(y.shape[0] + (slices[0] * slices[1] - y.shape[0] + current_example), 1),
-                                     dtype=numpy.float32)
-                temp[:y.shape[0]] = y
-
-                y = temp
-
-            for slice_x in range(slices[0]):
-                for slice_y in range(slices[1]):
-
-                    x[current_example] = \
-                        temp_image[
-                        slice_x * patch_size[0]:(slice_x + 1) * patch_size[0],
-                        slice_y * patch_size[1]:(slice_y + 1) * patch_size[1]
-                        ]
-                    y[current_example] = temp_score / 100
-                    current_example += 1
-            self.x = x
-            self.y = y
+        (self.train_x, self.train_y) = pickle.load(open(train_path, "rb"))
+        (self.val_x, self.val_y) = pickle.load(open(val_path, "rb"))
+        (self.test_x, self.test_y) = pickle.load(open(test_path, "rb"))
--- dataBaseCreation.py
+++ dataBaseCreation.py
@@ -1,50 +1,164 @@
 import numpy
 import matplotlib.image as img
-import matplotlib.pyplot as plt
+# for debug import matplotlib.pyplot as plt
 import Artifactory
 import os
 import data
+import shutil
 
 
 patch_size = 100
 max_angle = 10
+min_angle = 5
 num_of_examps = 10000
-artifact_prob = 0.25
+# percentages
+validation_examps = 0.1
+test_examps = 0.1
+artifact_prob = 0.5
 images_path = r"../Raw"
+
+path_to_validation_images = './images/val'
+path_to_traning_images = './images/train'
+path_to_test_images = './images/test'
 
 articactor = Artifactory.Artifactory()
 
-# articactor.set_image(input_image, patch_size, patch_size)
+try:
+    shutil.rmtree('./images')
+    os.mkdir('./images')
+except IOError:
+    pass
+try:
+    os.mkdir(path_to_validation_images)
+except IOError:
+    pass
+try:
+    os.mkdir(path_to_traning_images)
+except IOError:
+    pass
+try:
+    os.mkdir(path_to_test_images)
+except IOError:
+    pass
+try:
+    os.mkdir(os.path.join(path_to_validation_images, '0'))
+except IOError:
+    pass
+try:
+    os.mkdir(os.path.join(path_to_validation_images, '1'))
+except IOError:
+    pass
+
+try:
+    os.mkdir(os.path.join(path_to_traning_images, '0'))
+except IOError:
+    pass
+try:
+    os.mkdir(os.path.join(path_to_traning_images, '1'))
+except IOError:
+    print("Could not create folder:: " + os.path.join(path_to_traning_images, '1'))
+try:
+    os.mkdir(os.path.join(path_to_test_images, '0'))
+except IOError:
+    print("Could not create folder:: " + os.path.join(path_to_test_images, '0'))
+try:
+    os.mkdir(os.path.join(path_to_test_images, '1'))
+except IOError:
+    print("Could not create folder:: " + os.path.join(path_to_test_images, '1'))
 
 
 images_names = os.listdir(r"../Raw")
 data_set = data.Data('p025_10000')
 data_set_x = numpy.empty(shape=(num_of_examps, patch_size, patch_size, 3), dtype=numpy.float32)
-data_set_y = numpy.empty(shape=(num_of_examps, 1), dtype=numpy.float32)
+data_set_y = numpy.empty(shape=(num_of_examps, 2), dtype=numpy.float32)
 
 for im, image_name in enumerate(images_names):
     input_image = img.imread(os.path.join(images_path, image_name), 'tif')
-    for k in range(int(num_of_examps /len(images_names))):
+    for k in range(int(num_of_examps / len(images_names))):
         articactor.set_image(input_image, patch_size, patch_size)
         patch = articactor.get_random_patch()
 
+        y = numpy.zeros(shape=2, dtype=numpy.float32)
+        y[0] = 1.
 
-        y = 0
         # p probability for misalignment
         choise = numpy.random.choice(2, 1, p=[artifact_prob, 1-artifact_prob])
         if choise:
             choise2 = numpy.random.choice(2, 1, p=[0.5, 0.5])
             if choise2:
-                patch, _ = articactor.patch_misalignment(patch, 10, vertical_seam=True)
-            else:
-                patch, _ = articactor.patch_misalignment(patch, 10, vertical_seam=False)
-            y = 1
+                patch, seam = articactor.patch_misalignment(patch, min_angle, max_angle, vertical_seam=True)
 
-        data_set_x[im*int((num_of_examps /len(images_names))) + k, :, :, :] = patch
-        data_set_y[im*int((num_of_examps /len(images_names))) + k] = y
+                '''
+                    plt.subplot(1,2,1)
+                    plt.imshow(patch)
+                    plt.subplot(1,2,2)
+                    plt.imshow(seam*255)
+                    plt.show()
+                '''
+
+            else:
+                patch, seam = articactor.patch_misalignment(patch, min_angle, max_angle, vertical_seam=False)
+
+                '''
+                    plt.subplot(1,2,1)
+                    plt.imshow(patch)
+                    plt.subplot(1,2,2)
+                    plt.imshow(seam*255)
+                    plt.show()
+                '''
+
+            y[0] = 0.
+            y[1] = 1.
+
+        data_set_x[int(num_of_examps / len(images_names)) * im + k, :, :, :] = patch
+        data_set_y[int(num_of_examps / len(images_names)) * im + k, :] = y
+
         print('Image ' + image_name + ' patch number {0:d}, choise {1:d}'.format(
-            im*int((num_of_examps /len(images_names))) + k, int(choise)))
+            int(num_of_examps / len(images_names)) * im + k, int(choise)))
+
+
 data_set.x = data_set_x
 data_set.y = data_set_y
+# shuffle all examples
+data_set.shuffle()
+# split data to train, validation and test
+data_set.set_train_val_batch(int(validation_examps * num_of_examps), int(test_examps * num_of_examps))
 
-data_set.export_data('p025_10000.pickle')
+# save data to folders
+
+for k in range(data_set.val_x.shape[0]):
+    if data_set.val_y[k][0] == 1:
+        '''
+            NOT damaged images
+        '''
+        img.imsave(os.path.join(path_to_validation_images, '0', '{:04d}.bmp'.format(k)),
+                   data_set.val_x[k].astype(numpy.uint8))
+    else:
+        img.imsave(os.path.join(path_to_validation_images, '1', '{:04d}.bmp'.format(k)),
+                   data_set.val_x[k].astype(numpy.uint8))
+
+for k in range(data_set.train_x.shape[0]):
+    if data_set.train_y[k][0] == 1:
+        '''
+            NOT damaged images
+        '''
+        img.imsave(os.path.join(path_to_traning_images, '0', '{:04d}.bmp'.format(k)),
+                   data_set.train_x[k].astype(numpy.uint8))
+    else:
+        img.imsave(os.path.join(path_to_traning_images, '1', '{:04d}.bmp'.format(k)),
+                   data_set.train_x[k].astype(numpy.uint8))
+
+for k in range(data_set.test_x.shape[0]):
+    if data_set.test_y[k][0] == 1:
+        '''
+            NOT damaged images
+        '''
+        img.imsave(os.path.join(path_to_test_images, '0', '{:04d}.bmp'.format(k)),
+                   data_set.test_x[k].astype(numpy.uint8))
+    else:
+        img.imsave(os.path.join(path_to_test_images, '1', '{:04d}.bmp'.format(k)),
+                   data_set.test_x[k].astype(numpy.uint8))
+
+data_set.export_data(os.path.join(path_to_traning_images, 'train.pickle'),
+                     os.path.join(path_to_validation_images, 'val.pickle'),
+                     os.path.join(path_to_test_images, 'test.pickle'))
